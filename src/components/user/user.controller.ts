9ec8b5fd96e58e049d9aba3d671d65d8193abe53
nayefserag <nayfserag5@gmail.com>
2024-01-15 20:39:31 +0200

    eedit route

--- src/components/user/user.controller.ts
+++ src/components/user/user.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post, UsePipes, ValidationPipe  } from '@nestjs/common';
+import { Body, Controller, Post, Res, UsePipes, ValidationPipe  } from '@nestjs/common';
 import { UserDto } from 'src/dto/user.dto';
 import { UserService } from './user.service';
 import { Response } from 'express';
@@ -11,13 +11,13 @@ export class UserController {
  }
     @Post('create')
     @UsePipes(new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true })) // read about it don't forget
-    create(@Body() user: UserDto , res:Response) {
-        const exist = this.userService.getUser(user.email);
+    async create(@Body() user: UserDto ,@Res() res:Response) {
+        const exist = await this.userService.getUser(user.email);
         if (exist){
-            return res.status(409).json({message: 'User already exists'});
+            return res.json({message: 'User already exists', status: 409}).status(409);
         }
         const newuser = this.userService.createUser(user);
-        res.status(201).json({message: 'User created successfully' , status: 201 , data: newuser});
+        return res.status(201).json({message: 'User created successfully' , status: 201 , data: newuser});
         
     }
 
