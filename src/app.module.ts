6616c9385203365f205e314bae7b5896094d2791
nayefserag <nayfserag5@gmail.com>
2024-01-17 14:46:14 +0200

    Remove unnessecary module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './components/user/user.module';
-import { AccountModule } from './components/account/account.module';
+
 import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '@nestjs/config';
 @Module({
@@ -8,7 +8,6 @@ import { ConfigModule } from '@nestjs/config';
     ConfigModule.forRoot(),
     MongooseModule.forRoot(process.env.MONGO_URL),
     UserModule,
-    AccountModule,
   ],
 })
 export class AppModule {}
