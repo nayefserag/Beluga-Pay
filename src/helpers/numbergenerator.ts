72dd1d599110fa220be6f238c7a6a1084bd28ae8
nayefserag <nayfserag5@gmail.com>
2024-01-20 00:57:18 +0200

    fix small bug in helpers

--- src/helpers/numbergenerator.ts
+++ src/helpers/numbergenerator.ts
@@ -2,6 +2,6 @@ import { v4 } from 'uuid';
 
 export function generateAccountNumber() {
   const uuid = v4();
-  const numericString = uuid.replace(/-/g, '').replace(/\D/g, ''); // Remove non-numeric characters
+  const numericString = uuid.replace(/-/g, '').replace(/\D/g, '');
   return numericString.substring(0, 16);
 }
